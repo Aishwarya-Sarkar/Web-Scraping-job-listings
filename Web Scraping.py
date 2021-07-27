c4a2d7d8373ef0d7cd1656e93e7e12d087e5da75
Aishwarya Sarkar <81924922+Aishwarya-Sarkar@users.noreply.github.com>
2021-07-27 23:30:47 +0530

    Update Web Scraping.py

--- Web Scraping.py
+++ Web Scraping.py
@@ -4,6 +4,12 @@ import pandas as pd
 
 search_query = 'https://www.indeed.com/q-data-scientist-jobs.html'
 driver = webdriver.Chrome(executable_path='C:/Users/Aishwarya/Downloads/chromedriver.exe')
+
+driver.get(search_query)
+driver.maximize_window()
+
+time.sleep(5)
+
 job_details = []
 
 driver.get(search_query)
