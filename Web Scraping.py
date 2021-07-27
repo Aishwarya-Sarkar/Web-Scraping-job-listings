70566c215093084e92e5ad615da7837714997c06
Aishwarya Sarkar <81924922+Aishwarya-Sarkar@users.noreply.github.com>
2021-07-27 23:11:12 +0530

    Create Web Scraping.py

---
+++ Web Scraping.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+import pandas as pd
+
+search_query = 'https://www.indeed.com/q-data-scientist-jobs.html'
+driver = webdriver.Chrome(executable_path='C:/Users/Aishwarya/Downloads/chromedriver.exe')
+job_details = []
+
+driver.get(search_query)
+time.sleep(5)
+#job_list = driver.find_elements_by_xpath("//div[@class='job_seen_beacon']")
+company = driver.find_elements_by_xpath("//a[@data-tn-element ='companyName']")
+title = driver.find_elements_by_xpath("//span[@title='Data Scientist']")
+#print(job_company.text)
+print('Done')
